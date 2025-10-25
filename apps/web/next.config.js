589c12f390da7c413cecc0c48f8acd09eda6e906
dismod47 <dismod47@gmail.com>
2025-10-24 20:30:23 -0500

    chore: ensure no basePath/assetPrefix in next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,13 +1,8 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development'
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: { appDir: true },
+  // no basePath, no assetPrefix
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
